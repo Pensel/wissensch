13965ecb25c245a3d55de5c30e69738fd24f4566
Pensel <johannes.p.pensel@gmx.de>
2018-05-26 01:03:02 +0200

    collectionscript

--- collect.sh
+++ collect.sh
@@ -1,8 +1,8 @@
 SAVELOCATION="/media/data/collection/"
-PWD="/media/data/gits/StudiumNow/Wissenschaftliches\ arbeiten/Code/out/production/Code"
-DURATION="1s"
-timeout $DURATION java Test PRE >> $SAVELOCATION/collectPRE
-timeout $DURATION java Test POST >> $SAVELOCATION/collectPOST
+CLASSPATH="/media/data/gits/StudiumNow/WA/Code/out/production/Code"
+DURATION="1m"
+timeout $DURATION java -cp $CLASSPATH Test PRE >> $SAVELOCATION/collectPRE
+timeout $DURATION java -cp $CLASSPATH Test POST >> $SAVELOCATION/collectPOST
 grep "D" $SAVELOCATION/collectPRE > $SAVELOCATION/d.pre
 grep "B" $SAVELOCATION/collectPRE > $SAVELOCATION/b.pre
 grep "D" $SAVELOCATION/collectPOST > $SAVELOCATION/d.post
@@ -19,9 +19,9 @@ cat $SAVELOCATION/b.post.data | datamash median 1 > $SAVELOCATION/b.post.res
 cat $SAVELOCATION/d.post.data | datamash median 1 > $SAVELOCATION/d.post.res
 
 echo "BPRE:" >> $SAVELOCATION/final.res
-cat $SAVELOCATION/b.pre.res
+cat $SAVELOCATION/b.pre.res >> $SAVELOCATION/final.res
 echo "DPRE:" >> $SAVELOCATION/final.res
-cat $SAVELOCATION/d.pre.res
+cat $SAVELOCATION/d.pre.res >> $SAVELOCATION/final.res
 
 echo "BPOST:" >> $SAVELOCATION/final.res
 cat $SAVELOCATION/b.post.res >> $SAVELOCATION/final.res
