c1ce84dff93707cb0de4b48f17d2cba22fcb6102
Pensel <johannes.p.pensel@gmx.de>
2018-05-25 22:16:07 +0200

    negativer cycle test reformatiert (nicht mehr unidirektional negativ)

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test {
 
     private static int[][] testNegativerCycle = {{0, 1, 2, 0, 0},
             {1, 0, -7, 5, 0},
-            {2, -7, 0, 0, 7},
+            {2, 3, 0, 0, 7},
             {0, 5, 0, 0, 1},
             {0, 0, 7, 1, 0}};
     private static int[][] testNegativeKante = {{0, -1, 5, 0, 0},
