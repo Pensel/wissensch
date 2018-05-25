70955dc5d8481ca5dfb81df27b8702c0487adfdf
Pensel <johannes.p.pensel@gmx.de>
2018-05-25 19:10:01 +0200

    testBigger

--- Test.java
+++ Test.java
@@ -21,7 +21,17 @@ public class Test {
             {3, 0, 0, 0, 2},
             {0, 0, 0, 0, 1},
             {0, 0, 0, 1, 0}};
-    ;
+
+    private static int[][] testBigger = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+            {4, 0, 8, 0, 0, 0, 0, 11, 0},
+            {0, 8, 0, 7, 0, 4, 0, 0, 2},
+            {0, 0, 7, 0, 9, 14, 0, 0, 0},
+            {0, 0, 0, 9, 0, 10, 0, 0, 0},
+            {0, 0, 4, 14, 10, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 2, 0, 1, 6},
+            {8, 11, 0, 0, 0, 0, 1, 0, 7},
+            {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
 
 
     private static void divider()
@@ -54,6 +64,10 @@ public class Test {
 
         bfa.wrapper(new Graph(testDirected),0);
         d.dijkstra(testDirected,0);
+
+
+        bfa.wrapper(new Graph(testBigger),0);
+        d.dijkstra(testBigger,0);
 
     }
 }
